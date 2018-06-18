b2d5d245d17507a884c1c0c55f9954156304b0d8
Simon Prosser <pross@pross.org.uk>
2018-06-18 17:45:05 +0100

    Use modal, also count all domains on the server.

--- content.js
+++ content.js
@@ -7,43 +7,59 @@ chrome.runtime.onMessage.addListener(
 			bbtheme = window.location.href + '/wp-content/themes/bb-theme/changelog.txt'
 			themer = window.location.href + '/wp-content/plugins/bb-theme-builder/changelog.txt'
       free = window.location.href + '/wp-content/plugins/beaver-builder-lite-version/changelog.txt'
+      
+      url = window.location.href;
+      urlParts = url.replace('http://','').replace('https://','').split(/[/?#]/);
+      domain = urlParts[0];
 
 			var bboutput = false;
 
 			result = GetResult( bbplugin )
 			version = ParseResult( result )
 			if( version ) {
-				bboutput = 'Beaver Builder version ' + version + ' detected!\n'
+				bboutput = 'Beaver Builder version <strong>' + version + '</strong><br />'
 			} else {
-				bboutput = 'Beaver Builder not found.\n'
+				bboutput = 'Beaver Builder not found.<br />'
 			}
 
 			result = GetResult( bbtheme )
 			version = ParseResult( result )
 			if( version ) {
-				bboutput += 'Beaver Theme version ' + version + ' detected!\n'
+				bboutput += 'Beaver Theme version <strong>' + version + '</strong><br />'
 			} else {
-				bboutput += 'Beaver Theme not found.\n'
+				bboutput += 'Beaver Theme not found.<br />'
 			}
 
 			result = GetResult( themer )
 			version = ParseResult( result )
 			if( version ) {
-				bboutput += 'Beaver Themer version ' + version + ' detected!\n'
+				bboutput += 'Beaver Themer version <strong>' + version + '</strong><br />'
 			} else {
-				bboutput += 'Beaver Themer not found.\n'
+				bboutput += 'Beaver Themer not found.<br />'
 			}
       
       result = GetResult( free )
 			version = ParseResult( result )
 			if( version ) {
-				bboutput += 'Beaver Lite version ' + version + ' detected!\n'
+				bboutput += 'Beaver Lite version <strong>' + version + '</strong><br />'
 			} else {
-				bboutput += 'Beaver Lite not found.\n'
+				bboutput += 'Beaver Lite not found.<br />'
 			}
+      
+      lookup = 'https://api.hackertarget.com/reverseiplookup/?q=' + domain
+      domains = GetResult(lookup)
+      if( domains.length > 0 ) {
+        count = domains.split(/\r\n|\r|\n/).length - 1
+        bboutput += '<strong>' + count + '</strong> sites detected on this server.<br />'
+        url = 'https://www.yougetsignal.com/tools/web-sites-on-web-server/?remoteAddress=' + domain
+        bboutput += '<a target="_blank" href="' + url + '">List all domains.</a>'
+      }
 
 			if( bboutput ) {
-				alert( bboutput )
+        var myModal = new Modal({
+          title: 'Test Results for ' + domain,
+          content: bboutput
+        }).show();
 			}
     }
   }
